f3122b7dd2b5c8e57b089cff3fef867e73f585f2
Andrea Sottocornola <sottocornola.andrea@gmail.com>
2019-12-17 12:08:13 +0100

    working api requests

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+const localUrl = 'http://ec2-35-180-201-108.eu-west-3.compute.amazonaws.com:8080/deliveries';
+
 
 // @ts-ignore
 @Injectable({
   providedIn: 'root'
 })
-
-const localUrl = 'http://ec2-35-180-201-108.eu-west-3.compute.amazonaws.com:8080/deliveries';
 
 export class ApiService {
   constructor(private http: HttpClient) { }
